e639a6d0ba197bfb59cd4d0afed961b0a5c32b40
alexandr <95641302+vtoroiklass@users.noreply.github.com>
2022-12-21 03:11:37 +0600

    Add files via upload

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -5,6 +5,19 @@ const body = document.querySelector('body')
 const addSidebarBtn = document.querySelector('.add-sidebar-btn')
 let b = 0
 
+const showArticlesBtn = document.querySelector('.show-articles-btn')
+const articlesUl = document.querySelector('.articles-ul')
+
+showArticlesBtn.addEventListener('click', () => {
+    const articlesUlStyles = window.getComputedStyle(articlesUl)
+    if (articlesUlStyles.getPropertyValue('display') === 'block') {
+        articlesUl.style.display = 'none'
+        return
+    }
+
+    articlesUl.style.display = 'block'
+})
+
 addSidebarBtn.addEventListener('click', () => {
     if (b == 0) {
         sidebar.style.left = 0 + '%'
