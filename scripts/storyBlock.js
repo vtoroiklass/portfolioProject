0b82f905c54fc81b5915ac83690c4c27c325d3fb
alexandr <95641302+vtoroiklass@users.noreply.github.com>
2022-12-23 18:49:25 +0600

    Add files via upload

--- scripts/storyBlock.js
+++ scripts/storyBlock.js
@@ -1,39 +1,83 @@
-const story = document.querySelectorAll(".story-block-el");
-const storyName = document.querySelectorAll(".story-name");
-const storyDate = document.querySelectorAll(".date");
-const durationWrapper = document.querySelectorAll(".duration-wrapper");
-const duration = document.querySelectorAll(".duration");
-let durationWidth = 0;
-
-function closeStory() {}
+const story = document.querySelectorAll('.story-block-el')
 
 for (let i = 0; i < story.length; i++) {
-  story[i].addEventListener("click", (e) => {
-    story[i].classList.add("story-block-el-active");
-    storyName[i].style.display = "none";
-    storyDate[i].style.display = "none";
+  story[i].addEventListener('click', (e) => {
+    let durationWidth = 0
+    let counter = i
+
+    const storyImgs = document.querySelectorAll('.story-img')
+
+    const storyImgWrapper = document.createElement('div')
+    storyImgWrapper.classList.add('story-img-wrapper')
+    body.appendChild(storyImgWrapper)
+
+
+    const img = document.createElement('img')
+    img.classList.add('view-story')
+    storyImgWrapper.appendChild(img)
+    img.src = storyImgs[i].src
+
+    const durationWrapper = document.createElement('div')
+    durationWrapper.classList.add('duration-wrapper')
+    storyImgWrapper.appendChild(durationWrapper)
+
+    const duration = document.createElement('div')
+    duration.classList.add('duration')
+    durationWrapper.appendChild(duration)
+
+    const closeStoryBtn = document.createElement('button')
+    closeStoryBtn.innerHTML = 'X'
+    closeStoryBtn.classList.add('close-story-btn')
+    storyImgWrapper.appendChild(closeStoryBtn)
+    closeStoryBtn.addEventListener('click', () => {
+      storyImgWrapper.remove()
+      img.remove()
+      clearInterval(setDuration)
+      body.style.overflow = 'auto'
+      return
+    })
+
+    body.style.overflow = 'hidden'
+
     let setDuration = setInterval(() => {
-      if (duration[i].clientWidth > durationWrapper[i].clientWidth) {
-        clearInterval(setDuration);
-        story[i].classList.remove("story-block-el-active");
-        durationWidth = 0;
-        duration[i].style.width = durationWidth + "px";
-        storyName[i].style.display = "block";
-        storyDate[i].style.display = "block";
-        if (i < story.length - 1) {
-          story[i + 1].click();
-        }
-        return;
+      if (durationWidth < 100) {
+        durationWidth += 1
+        duration.style.width = durationWidth + '%'
+        return
       }
-      //   if (document.composedPath().includes(story)) {
-      // return;
-      // console.log(1)
-      //   }
-    //   story[i].onclick() = null;
-      durationWidth += 1;
-      duration[i].style.width = durationWidth + "px";
-      document.querySelector("body").style.overflowY = "auto";
-    }, 10);
-    document.querySelector("body").style.overflow = "hidden";
-  });
+      if (durationWidth == 100) {
+        if (i < story.length - 1) {
+          storyImgWrapper.remove()
+          img.remove()
+          story[i + 1].click()
+          clearInterval(setDuration)
+          return
+        }
+        clearInterval(setDuration)
+        storyImgWrapper.remove()
+        img.remove()
+        body.style.overflow = 'auto'
+        return
+      }
+    }, 30)
+
+    img.addEventListener('click', (e) => {
+      storyImgWrapper.remove()
+      img.remove()
+      clearInterval(setDuration)
+
+      if (counter + 1 === story.length) {
+        body.style.overflow = 'auto'
+        return
+      }
+
+      storyImgWrapper.addEventListener('mousedown', () => {
+        clearInterval(setDuration)
+        console.log(1)
+      })
+
+      counter++
+      story[counter].click()
+    })
+  })
 }
